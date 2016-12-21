9f9f27fa0084ac5beecd3338c0b9e1b5bd93b77a
Chris Wiseman <cjwiseman11@gmail.com>
2016-12-21 13:47:48 +0000

    Updated bot update ids as this was incorrect

--- repostBot.php
+++ repostBot.php
@@ -11,10 +11,10 @@ function connectToDatabase(){
     return $db;
 }
 
-function getLastUpdate($bot_id){
+function getLastUpdate(){
 	$db = connectToDatabase();
-	$statement = $db->prepare("SELECT * FROM `telegramBotFeed` WHERE `id` = :id");
-	$statement->execute(array(':id' => $bot_id));
+	$statement = $db->prepare("SELECT * FROM `telegramBotFeed` WHERE `id` = 1");
+	$statement->execute();
 	$row = $statement->fetchAll();
 	return $row[0]["lastupdate"];
 }
@@ -63,7 +63,10 @@ function renewLastUpdate($last_update){
 	$statement->execute(array(':lastupdate' => $last_update));
 }
 
-$bot_id = $config['botid'];
+$root = $_SERVER['DOCUMENT_ROOT'];
+$botconfig = parse_ini_file($root . '/../configTelegram.ini');
+
+$bot_id = $botconfig['botid'];
 $last_update = getLastUpdate($bot_id);
 
 echo "<br>Before: $last_update";
@@ -94,7 +97,6 @@ if ($last_update<$result["update_id"]){
 
 
 	$url_check = $result["message"]["entities"][0]["type"];
-
 	if($url_check == "url"){
 
 		//TODO: Utilise url entity when available
@@ -107,37 +109,37 @@ if ($last_update<$result["update_id"]){
 		//preg_match_all('#(www\.|https?:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\S*)#i', , $matches); Old Match
 		preg_match_all('#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#', $received_message, $matches);
 
+		file_get_contents('https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='.$url_exists.'&chat_id='.$chat_id.'&reply_to_message_id='.$original_msg_id);
 		foreach($matches[0] as $extracted_link){
 
-	      $url_repost = urlRepostCheck($extracted_link, $chat_id);
+      $url_repost = urlRepostCheck($extracted_link, $chat_id);
 
-	      $firstname = $result["message"]["from"]["first_name"];
-	      $messageid = $result["message"]["message_id"];
+      $firstname = $result["message"]["from"]["first_name"];
+      $messageid = $result["message"]["message_id"];
 
-	      if($url_repost){
+      if($url_repost){
 
-	        $message = "wowow repost lul";
-	        $original_msg_id = $url_repost["messageid"];
-	        file_get_contents('https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='.$message.'&chat_id='.$chat_id.'&reply_to_message_id='.$original_msg_id);
+        $message = "wowow repost lul";
+        $original_msg_id = $url_repost["messageid"];
+        file_get_contents('https://api.telegram.org/bot' . $bot_id . '/sendMessage?text='.$message.'&chat_id='.$chat_id.'&reply_to_message_id='.$original_msg_id);
 
-	        $repost_stat_check = checkRepostStats("exist", $firstname, $chat_id);
-					if($repost_stat_check)){
-						checkRepostStats("update", $firstname, $chat_id);
-	        } else {
-	            checkRepostStats("insert", $firstname, $chat_id);
-	        }
-	        echo "<br>repost";
-	  		} else {
-	            //Add URL to list
-	            echo "<br>not a repost, adding to db";
-							addUrlToDB($extracted_link, $firstname, $messageid, $chat_id);
-	            mysqli_query($connection,"");
-	      }
-	    }
-		}
+        $repost_stat_check = checkRepostStats("exist", $firstname, $chat_id);
+				if($repost_stat_check){
+					checkRepostStats("update", $firstname, $chat_id);
+        } else {
+            checkRepostStats("insert", $firstname, $chat_id);
+        }
+        echo "<br>repost";
+  		} else {
+            //Add URL to list
+            echo "<br>not a repost, adding to db";
+						addUrlToDB($extracted_link, $firstname, $messageid, $chat_id);
+            mysqli_query($connection,"");
+      }
+    }
 	}
 }
 $last_update = $result["update_id"];
 renewLastUpdate($last_update);
 echo "<br>After: $last_update";
-?>.
+?>
